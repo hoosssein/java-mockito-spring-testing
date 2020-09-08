c7aaf321a9dfcc8a40cc56a3910172426f7377c3
Hossein Nasr Esfahani <hoosssein@gmail.com>
2020-09-09 01:16:54 +0430

    5. introduce mock

--- src/test/java/testing/greet/GreeterTest.java
+++ src/test/java/testing/greet/GreeterTest.java
@@ -1,36 +1,39 @@
 package testing.greet;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import testing.writer.Writeable;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Created by hossein on 2020-09-09
  */
 class GreeterTest {
+    private final String ali = "Ali";
+
     @Test
     void greet() {
-        WriterStub writerStub = new WriterStub();
-        Scanner scannerStub = new Scanner("Ali");
-        Greeter greeter = new Greeter(writerStub,scannerStub);
+        WriterMock writerMock = new WriterMock();
+        Scanner scannerStub = new Scanner(ali);
+        Greeter greeter = new Greeter(writerMock, scannerStub);
 
         greeter.greet();
 
-        assertEquals(1, writerStub.printed.size());
-        assertEquals("Hello, Ali", writerStub.printed.get(0));
+        assertTrue(writerMock.success);
     }
-}
 
-class WriterStub implements Writeable {
-    List<String> printed = new ArrayList<>();
 
-    @Override
-    public void println(String s) {
-        printed.add(s);
+    class WriterMock implements Writeable {
+        boolean success = false;
+
+        @Override
+        public void println(String s) {
+            if (("Hello, " + ali).equals(s)) {
+                success = true;
+            }
+        }
     }
 }
