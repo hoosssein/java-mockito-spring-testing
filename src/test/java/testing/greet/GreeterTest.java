2bb011b1b3c72b034345594c01056b11cc86719a
Hossein Nasr Esfahani <hoosssein@gmail.com>
2020-09-09 02:01:24 +0430

    8. use mockito with RunWith

--- src/test/java/testing/greet/GreeterTest.java
+++ src/test/java/testing/greet/GreeterTest.java
@@ -1,33 +1,44 @@
 package testing.greet;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import testing.writer.Writeable;
 
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 
 /**
  * Created by hossein on 2020-09-09
  */
+@RunWith(JUnitPlatform.class)
+@ExtendWith(MockitoExtension.class)
+//@RunWith(MockitoJUnitRunner)
 class GreeterTest {
     private final String ali = "Ali";
 
+    @Mock
+    Writeable writerMock;
+    @Mock
+    Scanner scannerMock;
+    @InjectMocks
+    Greeter greeter;
+
     @Test
     void greet() {
-        Writeable writerMock = Mockito.mock(Writeable.class);
-
-        Scanner scannerMock = Mockito.mock(Scanner.class);
         Mockito.when(scannerMock.nextLine()).thenReturn(ali);
-
-        Greeter greeter = new Greeter(writerMock, scannerMock);
 
         greeter.greet();
 
         Mockito.verify(writerMock).println(eq("Hello, "+ali));
         Mockito.verify(scannerMock).nextLine();
+        Mockito.verifyNoMoreInteractions(writerMock,scannerMock);
     }
 }
