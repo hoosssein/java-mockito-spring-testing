b1ebeaa4e3bcdb252f6f021d8ff32c9d1161f0ce
Hossein Nasr Esfahani <hoosssein@gmail.com>
2020-09-09 00:05:44 +0430

    Program

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import testing.Greeter;
+
+/**
+ * Created by hossein on 2020-09-09
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Greeter().greet();
+    }
+}
